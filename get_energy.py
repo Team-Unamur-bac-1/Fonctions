fb70d42f0dd52ef965c989a6f54c649186909054
carolineheijmans <62341105+carolineheijmans@users.noreply.github.com>
2020-03-27 18:59:15 +0100

    Add files via upload

---
+++ get_energy.py
@@ -0,0 +1,21 @@
+def get_energy(player):
+    """
+    Check the amount of energy of a player, returns this amount
+    
+    Parameters
+    ----------
+    player : name of the player who need energy (str)
+    
+    Returns
+    _____
+    amount_energy : amount of energy in hub of the player (int)
+ 
+    Version
+    ______
+    Specification : Marie Heneffe (v.1 24/02/2020)
+    Specification : Caroline Heijmans (v.2 27/03/2020)
+    Implementation : Caroline Heijmans (v.1 27/03/2020)
+    """   
+
+    return info_player[hub][energy]
+
